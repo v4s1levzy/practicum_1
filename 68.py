ccd61215967699383a7a77fb958843d8f643b613
v4s1levzy <73242538+v4s1levzy@users.noreply.github.com>
2020-12-20 21:38:38 +0500

    Add files via upload

---
+++ 68.py
@@ -0,0 +1,57 @@
+import random
+
+N = 2  # строк
+M = 3  # столбцов
+
+
+def get_row(column):
+    col = []
+    for i in range(0, column):
+        col.append(random.randint(0, 9))
+
+    return col
+
+
+def get_matrix(row, column):
+    matrix = []
+    for i in range(0, row):
+        matrix.append(get_row(column))
+
+    return matrix
+
+
+def print_matrix(matrix):
+    i = 0
+    while i < len(matrix):
+        j = 0
+        row = matrix[i]
+        while j < len(row):
+            column = row[j]
+            print(column, end=' ')
+            j += 1
+
+        print()
+        i += 1
+
+
+def get_average(row):
+    sum = 0
+    for element in row:
+        sum += element
+
+    return sum/len(row)
+
+
+A = get_matrix(N, M)
+print("Матрица:")
+print_matrix(A)
+
+n = 0
+for row in A:
+    average = get_average(row)
+    if average > n:
+        n = average
+
+
+print("Наибольшее значение среди средних значений для каждой строки "
+      "матрицы:", n)
